ac1f1da599514543498f72b1aa57fd90bb06f28f
KatherineJA <136206274+KatherineJA@users.noreply.github.com>
2026-06-13 14:31:56 -0600

    Cambio a render del back

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cletaeatsjk.up.railway.app',
+  baseURL: 'https://cletaeats-backend-rgn1.onrender.com/',
   headers: {
     'Content-Type': 'application/json'
   }
